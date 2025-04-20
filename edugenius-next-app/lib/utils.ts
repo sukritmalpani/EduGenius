de446efb03f94492311400a08c8a0feb1d9afc97
Arpit1033 <lcb2022015@iiitl.ac.in>
2025-04-20 19:24:56 +0530

    Resolve Merge Conflict

--- edugenius-next-app/lib/utils.ts
+++ edugenius-next-app/lib/utils.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -9,15 +8,3 @@ export function cn(...inputs: ClassValue[]) {
 export function ny(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-=======
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-export function ny(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
->>>>>>> 2d59e9c0b21cc462c43c80c3de0bebb54f7081c5
