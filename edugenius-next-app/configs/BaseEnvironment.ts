398c0fb1381ab8155e1836f105900114dd312b5b
malpanisukrit <malpanisukrit@gmail.com>
2025-05-24 23:25:37 +0530

    feat: enhance BaseEnvironment to support environment variables for configuration; update db.ts to utilize new environment settings

--- edugenius-next-app/configs/BaseEnvironment.ts
+++ edugenius-next-app/configs/BaseEnvironment.ts
@@ -19,4 +19,85 @@ export class BaseEnvironment {
     FIREBASE_MEASUREMENT_ID: "G-6SG8WQ3PPV",
     YOUTUBE_API_KEY: "AIzaSyD9Al_kkxgoSF-tzrRq_YOXYy975wAlqSM",
   };
+
+  get environment(): Environment {
+    return process.env.NODE_ENV as Environment;
+  }
+
+  get HOST_URL(): string {
+    return (
+      process.env.NEXT_PUBLIC_HOST_URL! ||
+      this.defaultEnvironmentValues.HOST_URL
+    );
+  }
+
+  get GOOGLE_GEMENI_API_KEY(): string {
+    return (
+      process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY! ||
+      this.defaultEnvironmentValues.GOOGLE_GEMENI_API_KEY
+    );
+  }
+
+  get DRIZZLE_DATABASE_URL(): string {
+    return (
+      process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL! ||
+      this.defaultEnvironmentValues.DRIZZLE_DATABASE_URL
+    );
+  }
+
+  get FIREBASE_API_KEY(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_API_KEY! ||
+      this.defaultEnvironmentValues.FIREBASE_API_KEY
+    );
+  }
+
+  get FIREBASE_AUTH_DOMAIN(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN! ||
+      this.defaultEnvironmentValues.FIREBASE_AUTH_DOMAIN
+    );
+  }
+
+  get FIREBASE_PROJECT_ID(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID! ||
+      this.defaultEnvironmentValues.FIREBASE_PROJECT_ID
+    );
+  }
+
+  get FIREBASE_STORAGE_BUCKET(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET! ||
+      this.defaultEnvironmentValues.FIREBASE_STORAGE_BUCKET
+    );
+  }
+
+  get FIREBASE_MESSAGING_SENDER_ID(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID! ||
+      this.defaultEnvironmentValues.FIREBASE_MESSAGING_SENDER_ID
+    );
+  }
+
+  get FIREBASE_APP_ID(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_APP_ID! ||
+      this.defaultEnvironmentValues.FIREBASE_APP_ID
+    );
+  }
+
+  get FIREBASE_MEASUREMENT_ID(): string {
+    return (
+      process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID! ||
+      this.defaultEnvironmentValues.FIREBASE_MEASUREMENT_ID
+    );
+  }
+
+  get YOUTUBE_API_KEY(): string {
+    return (
+      process.env.NEXT_PUBLIC_YOUTUBE_API_KEY! ||
+      this.defaultEnvironmentValues.YOUTUBE_API_KEY
+    );
+  }
 }
--- edugenius-next-app/configs/db.ts
+++ edugenius-next-app/configs/db.ts
@@ -4,4 +4,5 @@ import { BaseEnvironment } from "./BaseEnvironment";
 
 const env = new BaseEnvironment();
 
-
+const sql = neon(env.DRIZZLE_DATABASE_URL!);
+export const db = drizzle(sql);
